82eb1bee08b710cadb48194ce41e2f62acd83756
celeska <celestin.kaske@iubh-fernstudium.de>
2024-02-12 23:43:16 +0100

    [All pages] = fixed the navbar to make it responsive and adjusted the content and padding for mobile

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function flipCard(card) {
   card.classList.toggle("flipped");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
+  const navLinks = document.getElementById("navLinks");
+
+  toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
