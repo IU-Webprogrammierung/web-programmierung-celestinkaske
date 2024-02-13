015829819659a24373675e3f85949bd36cdbbebb
celeska <celestin.kaske@iubh-fernstudium.de>
2024-02-13 13:51:07 +0100

    [All pages] = update the media queries for the navbar and the px paddings

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ function flipCard(card) {
   card.classList.toggle("flipped");
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.getElementById("toggleButton");
-  const navLinks = document.getElementById("navLinks");
+const toggleButton = document.getElementById("toggleButton");
+const navLinks = document.getElementById("navLinks");
 
-  toggleButton.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
+toggleButton.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+
+  const menuEl = document.getElementById("hamburger-menu");
+  const closeEl = document.getElementById("close-menu");
+
+  const isMenuOpen = navLinks.classList.contains("active");
+
+  if (isMenuOpen) {
+    menuEl.style.display = "none";
+    closeEl.style.display = "block";
+  } else {
+    menuEl.style.display = "block";
+    closeEl.style.display = "none";
+  }
 });
